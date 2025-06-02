65d9f2f035de8afdf626eb1a1c509ab4dd9d08ee
yousbot <sbaiidrissiyoussef@gmail.com>
2025-06-02 12:00:33 +0100

    Blank research version-01 update-02

--- global/scripts/script.js
+++ global/scripts/script.js
@@ -1,7 +1,11 @@
 function showContent(id) {
-    ['vision', 'products'].forEach(section => {
-        document.getElementById(section).classList.add('hidden')
-    })
-    document.getElementById(id).classList.remove('hidden')
+    const sections = ["vision", "products", "about-us"];
+    sections.forEach(sec => {
+        const el = document.getElementById(sec);
+        if (el) el.classList.add("hidden");
+    });
+    const target = document.getElementById(id);
+    if (target) target.classList.remove("hidden");
 }
+
 window.onload = () => showContent('vision');
